44a2adab5aeff4a5751a3f521fabb6b1d715b103
Dan2ie0l <daniel.harutyunyan.20@gmail.com>
2021-03-10 22:43:22 +0400

    Add project files.

---
+++ SimpleCipher/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace SimpleCipher
+{
+    class Program
+    {
+        public static string  plainAlphabet = "abcdefghijklmnopqrstuvwxyz";
+        static void Main(string[] args)
+        {
+            Console.Write("Text:");
+            string text = Console.ReadLine();
+            
+            string cipherAlphabet = "yhkqgvxfoluapwmtzecjdbsnri";
+
+            Console.Write("Choose(encipher/decipher):");
+            string choose = Console.ReadLine();
+
+            switch (choose)
+            {
+                case "encipher":
+                    Console.WriteLine(Encipher(text, cipherAlphabet) ); 
+                    break;
+
+                case "decipher":
+                    Console.Write("Ciphered Text:");
+                    string cipheredText = Console.ReadLine();
+                    Console.WriteLine(Decipher(cipheredText, cipherAlphabet));
+                    break;
+                case "e":
+                    Console.WriteLine(Encipher(text, cipherAlphabet));
+                    break;
+
+                case "d":
+                    Console.Write("Ciphered Text:");
+                    string cipherText = Console.ReadLine();
+                    Console.WriteLine(Decipher(cipherText, cipherAlphabet));
+                    break;
+            }
+
+          
+           
+
+        }
+
+        private static string Cipher(string input, string newAlphabet, string oldAlphabet)
+        {
+            string output = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                int oldCharIndex = oldAlphabet.IndexOf(char.ToLower(input[i]));
+
+                if (oldCharIndex >= 0)
+                {
+                    output += char.IsUpper(input[i]) ? char.ToUpper(newAlphabet[oldCharIndex]) : newAlphabet[oldCharIndex];
+                }
+
+                else
+                {
+                    output += input[i];
+                }
+            }
+
+            return output;
+
+        }
+
+        public static string Encipher(string input, string cipherAlphabet)
+        {
+           
+            return Cipher(input, plainAlphabet, cipherAlphabet);
+        }
+
+        public static string Decipher(string input, string cipherAlphabet)
+        {
+           
+            return Cipher(input, cipherAlphabet, plainAlphabet);
+        }
+    }
+
+
+
+}
